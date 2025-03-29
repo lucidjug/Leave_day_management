b89f865b7b66090cd9d22ecd004fb0b64fa64f91
quynhdang <quynh.dangthi2212880@hcmut.edu.vn>
2025-03-29 09:31:23 +0700

    fix something

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,52 +1,53 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./Profile.css";
 import { Button, Input, Form, Modal } from "antd";
 import { KeyOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import avatar from "../../assets/images/default-avatar.png";
+import { AuthContext } from "../../context/AuthContext";
 
 dayjs.extend(customParseFormat);
 
 // const dateFormat = "YYYY-MM-DD";
 
 export default function Profile() {
-  const [formData, setFormData] = useState({
-    name: "Nguyen Van A",
-    birthDate: "2000-01-01",
-    mssv: "1234567",
-    email: "vana.nguyen@example.com",
-    username: "nguyenvana",
-    role: "ADMIN",
-    leaveDays: 12,
-  });
+ const { user } = useContext(AuthContext);
+ console.log(user);
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [password, setPassword] = useState({
-    oldPassword: "",
-    password: "",
-  });
-  const [isEditing, setIsEditing] = useState(false);
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  //   mssv: "",
+  //   email: "",
+  //   role: "ADMIN",
+  // });
 
-  const handleEditClick = () => {
-    setIsEditing(!isEditing);
-  };
+  // const [isModalVisible, setIsModalVisible] = useState(false);
+  // const [password, setPassword] = useState({
+  //   oldPassword: "",
+  //   password: "",
+  // });
+  // const [isEditing, setIsEditing] = useState(false);
 
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
+  // const handleEditClick = () => {
+  //   setIsEditing(!isEditing);
+  // };
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-    setPassword({ oldPassword: "", password: "" });
-  };
+  // const showModal = () => {
+  //   setIsModalVisible(true);
+  // };
 
-  const handleInputChange = (name) => (event) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: event.target.value,
-    }));
-  };
+  // const handleCancel = () => {
+  //   setIsModalVisible(false);
+  //   setPassword({ oldPassword: "", password: "" });
+  // };
+
+  // const handleInputChange = (name) => (event) => {
+  //   setFormData((prevData) => ({
+  //     ...prevData,
+  //     [name]: event.target.value,
+  //   }));
+  // };
 
   // const handleDateChange = (date) => {
   //   setFormData((prevData) => ({
@@ -54,6 +55,8 @@ export default function Profile() {
   //     birthDate: date ? date.format(dateFormat) : "",
   //   }));
   // };
+
+
 
   return (
     <div id="wrapper">
@@ -69,16 +72,16 @@ export default function Profile() {
             </div>
             <div className="email-container">
               <div>
-                {formData.lastName} {formData.firstName}
+                {user.name}
               </div>
-              <p className="email">{formData.email}</p>
+              <p className="email">{user.email}</p>
             </div>
-            <div className="change-password">
+            {/* <div className="change-password">
               <Button onClick={handleEditClick}>Edit</Button>
               <Button onClick={showModal}>
                 <KeyOutlined /> Change password
               </Button>
-            </div>
+            </div> */}
           </div>
 
           <Form layout="vertical">
@@ -86,36 +89,36 @@ export default function Profile() {
               <Input
                 // style={{ width: "50%" }}
                 placeholder="Name"
-                value={formData.name}
-                onChange={handleInputChange("Name")}
-                disabled={!isEditing}
+                value={user.name}
+                // onChange={handleInputChange("Name")}
+                disabled={true}
               />
             </Form.Item>
 
             <Form.Item label="ID">
-              <Input value={formData.mssv} disabled />
+              <Input value={user.id} disabled />
             </Form.Item>
-
+{/* 
             <Form.Item label="Number of leave days">
-              <Input value={formData.leaveDays} disabled />
-            </Form.Item>
+              <Input value={user.leaveDays} disabled />
+            </Form.Item> */}
 
             <Form.Item label="Email">
-              <Input value={formData.email} disabled />
+              <Input value={user.email} disabled />
             </Form.Item>
 
-            <Form.Item label="Username">
-              <Input value={formData.username} disabled />
+            <Form.Item label="Role">
+              <Input value={user.role} disabled />
             </Form.Item>
 
-            <Form.Item style={{ textAlign: "center", marginTop: "10px" }}>
+            {/* <Form.Item style={{ textAlign: "center", marginTop: "10px" }}>
               <Button type="primary" disabled={!isEditing}>
                 Lưu thay đổi
               </Button>
-            </Form.Item>
+            </Form.Item> */}
           </Form>
         </div>
-        <Modal
+        {/* <Modal
           title="Đổi mật khẩu"
           visible={isModalVisible}
           onOk={handleCancel}
@@ -141,7 +144,7 @@ export default function Profile() {
               />
             </Form.Item>
           </Form>
-        </Modal>
+        </Modal> */}
       </div>
     </div>
   );
